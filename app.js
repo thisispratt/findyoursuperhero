5ee8112c35a74e219b41d064c46e1c23069df05d
prabhat tiwari <prabhat.avb@gmail.com>
2021-01-23 00:50:01 +0530

    first version

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodeFetch = require('node-fetch');
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var api_URL = "https://super-search-akashjoshi.flexiple.now.sh/?hero=";
+
+app.get("/home", function(req, res){
+    api_URL = "https://super-search-akashjoshi.flexiple.now.sh/?hero=";
+    res.render("home.ejs");
+});
+
+app.post("/hero", function(req, res){
+    api_URL += req.body.superHeroName;
+    res.redirect("/hero");
+});
+
+app.get("/hero", async function(req, res){
+
+    //Unhandled promise rejection to be done.
+    const response = await nodeFetch(api_URL).catch(function(err){});
+    const data = await response.json();
+
+    res.render("hero.ejs",{heroData: data});
+});
+
+app.listen(3000, () => console.log("running at 3000"));
+
